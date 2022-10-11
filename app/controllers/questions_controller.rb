02952c270ea9e8f01f16550486676f5664e4587e
Tim Weyel <tweyel@gmail.com>
2022-10-11 11:31:18 -0700

    add list of all questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,31 @@
 class QuestionsController < ApplicationController
   before_action :set_questions
-  before_action :set_question, only: [:show]
+  before_action :set_question, only: [:show, :index]
 
-  def index;end
+  def index
+    @questions = Question.all
+  end
 
-  def show;end
+  def show
+    byebug
+    @question = Question.find(params[:id])
+  end
+
+end
+
+  def new
+    @questions = Question.new
+  end
+
+  def create
+    @questions = Question.new(params.require(:question))
+    if @questions.save
+      flash[:notice] = "Question was created successfully."
+      redirect_to @questions
+    else
+      render 'new'
+    end
+  end
 
   private
 
@@ -15,4 +36,3 @@ class QuestionsController < ApplicationController
   def set_question
     @questions = @questions.sample
   end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'index#index'
-  get 'questions/:id', to: 'questions#index'
-  get "/get_question", to: "questions#show"
+  root "index#index"
+  get "questions/:id", to: "questions#index"
+  get "get_question", to: "questions#show"
+  post "new_question", to: "questions#new"
   resources :questions, only: [:show, :index]
 end
