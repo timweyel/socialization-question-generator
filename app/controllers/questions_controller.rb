bc7102cb3bd5d30515efbd4fe98d9c53f9b555e1
Tim Weyel <tweyel@gmail.com>
2022-10-13 15:07:50 -0700

    better show.html.erb fix

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
   # before_action :show, only: [:show, :index, :new, :create]
 
   def show
-    @questions = Question.find(params[:id])
+    @question = Question.find(params[:id])
   end
 
   def index
